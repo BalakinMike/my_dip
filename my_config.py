db5bb0140f79b5df52860a47e8e2d85dd233b6ea
BalakinMike <mikb2220@gmail.com>
2022-12-13 11:39:15 +0300

    first working release

---
+++ my_config.py
@@ -0,0 +1,10 @@
+user_token = 'vk1.a.vAy286_aUBXG82rsYyKVEli6wo0wll4wsgsy_C-_XV82SiGJIukkqAZoxj0VKNu5DfsmcBi6B8w8pH76Pg7-FirHNJ97oBvRFbyN20r31RGIvRcNltzuV_rKnltCw_g_0VFyFW1gFoMErlvtYGRrS109Hv_K7U-XRX-mssgJyErIwa0tchFGTBq5FIbRHa0e' # токен пользователя
+comm_token = 'vk1.a.j9E4AHS63egttHWC2gNSbJG9QubOLx60-tcX_Rr1nKwwieKRG3pDq6ZiSanldOzLdIL00_Mix1uMv2Hw65vMDSpTSqvFIqxOyhBqt7l93CQwkw6T9k-XFhZ1Fm0mV3vIyzSn0cQk1fqjpa_mKGsYqcWc2wj3IrV7cjOF5NC0SnnLU8Z-CcqToOhU7PqLrc6FSSoHlm_JhLvrIY5-VCt9Xg' # токен сообщества
+
+offset = 0 #сдвиг
+line = range(0, 1000) # последовательность для перебора найденных пользователей
+
+host = '127.0.0.1' 
+user = 'postgres'
+password = 'Mikmik38'
+db_name = 'vk_base'
